ae3e23c8fbc911e34ce65cd7d9f62fef42a128c6
songjumyung <jmsong620@gmail.com>
2025-03-13 06:00:48 +0900

    Quiz2

--- Math.fs
+++ Math.fs
@@ -6,4 +6,9 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let v = uint64 n
+  let rec iter (value : uint64) cnt = 
+    if value = (uint64 1) then cnt
+    elif value % (uint64 2) = (uint64 0) then iter (value / (uint64 2)) cnt + 1
+    else iter (value * (uint64 3) + (uint64 1)) cnt + 1
+  iter v 0
